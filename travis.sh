cd100855a3b8000fa52ca43878405c69372dc591
Henry Norris <henry.norris@ibm.com>
2021-03-19 15:35:13 -0400

    Fix Travis

--- travis.sh
+++ travis.sh
@@ -3,8 +3,8 @@
 git remote remove origin
 git remote remove upstream
 
-git remote add origin https://${DETECTIR_PERSONAL_TOKEN}@github.ibm.com/${TRAVIS_REPO_SLUG}
-git remote add upstream https://${DETECTIR_PERSONAL_TOKEN}@github.ibm.com/${TRAVIS_REPO_SLUG}.wiki
+git remote add origin git@github.ibm.com:${TRAVIS_REPO_SLUG}
+git remote add upstream git@github.ibm.com:${TRAVIS_REPO_SLUG}.wiki
 
 git pull origin master --no-edit
 git push upstream master --force
